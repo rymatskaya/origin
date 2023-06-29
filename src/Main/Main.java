fab00228ff82331adf3370cdbe6d7366a2180f59
Irina Rymatskaya <irina.rymatskaya@gmail.com>
2023-06-29 23:16:20 +0300

    * Создать два класса (в разных пакетах), первый - только для запуска второй класс - Радуга.         * Класс Радуга должен уметь выводить названия цветов по введенным номерам цветов.         * (сопоставление цвет-номер на ваше усмотрение).         * Класс Радуга может выводить не только основные цвета но еще и полуцвета         * (например, желто-зеленый);         * Способ задания номеров цвета придумать самостоятельно.         * Использовать для решения задачи if и switch.         * Методы можно вызывать друг из друга. Постарайтесь уменьшить дублирование кода.         *  Обратить внимание на именование методов, классов, переменных и констант.

---
+++ src/Main/Main.java
@@ -0,0 +1,36 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+package Main;
+import Rainbow.Rainbow;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Rainbow rainbow = new Rainbow();
+        rainbow.setColor(5);
+        System.out.println(rainbow.getColor());
+        System.out.println("Введите тон радуги? (от 0 до 6)");
+        int color = in.nextInt();
+        System.out.println("Введите полутон радуги? (от 0 до 6)");
+        int ton = in.nextInt();
+
+        System.out.println("Основной тон:");
+        System.out.println(rainbow.getColor(color));
+        System.out.println("Основной с оттенком:");
+        System.out.println(rainbow.getColor(ton,color));
+        /*
+        * Создать два класса (в разных пакетах), первый - только для запуска второй класс - Радуга.
+        * Класс Радуга должен уметь выводить названия цветов по введенным номерам цветов.
+        * (сопоставление цвет-номер на ваше усмотрение).
+        * Класс Радуга может выводить не только основные цвета но еще и полуцвета
+        * (например, желто-зеленый);
+        * Способ задания номеров цвета придумать самостоятельно.
+        * Использовать для решения задачи if и switch.
+        * Методы можно вызывать друг из друга. Постарайтесь уменьшить дублирование кода.
+        *  Обратить внимание на именование методов, классов, переменных и констант.
+        */
+
+
+    }
+}
---
+++ src/Rainbow/Rainbow.java
@@ -0,0 +1,72 @@
+package Rainbow;
+public class Rainbow {
+int color;
+
+    public Rainbow() {
+
+    }
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public void setColor(int color1, int color2) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public String getColor(int color)
+    {
+        String text;
+        switch (color) {
+            case 0:
+                text="Красный";
+                break;
+            case 1:
+                text="Оранжевый";
+                break;
+            case 2:
+                text="Жёлтый";
+                break;
+            case 3:
+                text="Зелёный";
+                break;
+            case 4:
+                text="Голубой";
+                break;
+            case 5:
+                text="Синий";
+                break;
+            case 6:
+                text="Фиолетовый";
+                break;
+            default:
+                text="";
+        }
+        return text;
+    }
+
+    public String getColor(int color1, int color2)
+    {
+        String text;
+
+        text=getColor(color1);
+        String subs=text.substring(text.length()-2);
+
+        if (subs.compareTo("ый")==0)
+        {
+            text.replace("ый", "о-");
+        } else
+        if (subs.compareTo("ий")==0)
+        {
+            text.replace("ий", "е-");
+        } else
+        {
+            text.replace("ой", "о-");
+        }
+
+        return text+getColor(color2);
+    }
+}
