5debb31eca3cc233d6b09c37808f1824287ee8e9
Irina Rymatskaya <irina.rymatskaya@gmail.com>
2023-08-02 00:10:06 +0300

    HashMap

---
+++ src/lesson10HashMap/Person.java
@@ -0,0 +1,50 @@
+package lesson10HashMap;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Person {
+    private UUID id;
+    protected String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id)  ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Person(UUID id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/lesson10HashMap/Runner.java
@@ -0,0 +1,54 @@
+package lesson10HashMap;
+
+import java.util.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        Person ivan = new Person(UUID.randomUUID(), "Ivan", 24);
+        Person nikita = new Person(UUID.randomUUID(), "Nikita", 24);
+
+        System.out.println(ivan.hashCode());
+        System.out.println(nikita.hashCode());
+
+        List<String> list = new LinkedList<>();
+        list.add("Hello");
+        Integer x = 10;
+        x.hashCode();
+
+        Map<Integer, String> map = new HashMap<>(25, 0.9F);
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(1, "three"); //перетрет первое значение
+        map.put(x, "ten");
+
+        System.out.println(map.get(10));
+
+        Map.Entry[] entry = new Map.Entry[16];
+
+        Person Ivan = new Person("Ivan", 24);
+        Person Sveta = new Person("Sveta", 26);
+
+        Map<String, Person> people = new HashMap<>();
+        people.put("ivan", Ivan);
+        people.put("sveta", Sveta);
+        people.put("jon", new Person("Jon", 15));
+
+        people.keySet().forEach(System.out::println);
+
+        for (Person value : people.values()) {
+            if (value.getAge() > 20) {
+                System.out.println(value.getName());
+            }
+        }
+
+        for (Map.Entry<String,Person> s : people.entrySet()) {
+            System.out.println(s.getKey());
+            System.out.println(s.getValue());
+        }
+
+        Person person = people.getOrDefault("Jon", new Person("default",0));
+        System.out.println(person.getName());
+
+
+    }
+}
